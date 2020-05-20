c70b159de56b385d02d55cac9838b9b6585155aa
shona <f.mb@outlook.jp>
2020-05-20 21:09:39 +0900

    txif set

--- js/main.js
+++ js/main.js
@@ -40,7 +40,39 @@ var firebaseConfig = {
 
 //  ボタン送信
     fileButton.addEventListener("change",function(e){   //ファイルを選択→送信でput〇
-        console.log(e)
+        
+        var file_input = $('#fileButton');
+        console.log(file_input);
+        
+        var file = file_input[0].files[0];
+        // EXIF.getDataでexif情報を解析
+        EXIF.getData(file, function() {
+            let data = EXIF.getAllTags(this);
+            console.log(data);
+            
+            let lat0 = data.GPSLatitude[0].numerator;
+            let lat1 = data.GPSLatitude[1].numerator;
+            let lat2 = data.GPSLatitude[2].numerator ;
+
+            let lat1A = lat1 / 60 
+            let lat2A = lat2 / 360000
+            
+            let lat = lat0 + lat1A + lat2A
+            console.log(lat);
+
+
+              
+            let lon0 = data.GPSLongitude[0].numerator;
+            let lon1 = data.GPSLongitude[1].numerator;
+            let lon2 = data.GPSLongitude[2].numerator;
+
+            let lon1A = lon1 / 60 
+            let lon2A = lon2 / 360000
+
+            let lon = lon0 + lon1A + lon2A
+            console.log(lon);
+    })
+        
 
         $('img').remove();
         var file = $(this).prop('files')[0];
@@ -60,36 +92,40 @@ var firebaseConfig = {
     //画像うｐ
     //ファイル名を取得
     let file = e.target.files[0];
-   
+        
+        
     //storage ref を作成 //ファイルのアップロード
     let storageRef = firebase.storage().ref('shon_wolrd/').child(file.name).put(file).then(snapshot => {
         let imgpath = firebase.storage().ref('shon_wolrd/').child(file.name).fullPath
+       
         let url = snapshot.ref.getDownloadURL().then(url =>{
+           
             const path = file.name;
             const imgUrl = url;
-    const uname = $("#uname").val();
-    const text = $("#text").val();
+
+            const uname = $("#uname").val();
+            const text = $("#text").val();
+
             let now = new Date();  //Time取得
-            let getMonth = now.getMonth();
-            let getDate = now.getDate();
-            let timeH = now.getHours();     //時間
-            let timeM = now.getMinutes();      //分
-                if(timeH<10){timeH ="0" + timeH}     //数字が一桁の場合０をつける。
-                if(timeM<10){timeM ="0" + timeM}
-    const time = `${getMonth}/${getDate}   ${timeH}:${timeM}`;         //時間：分を"time"に入れる
+                let getMonth = now.getMonth();
+                let getDate = now.getDate();
+                let timeH = now.getHours();     //時間
+                let timeM = now.getMinutes();      //分
+                    if(timeH<10){timeH ="0" + timeH}     //数字が一桁の場合０をつける。
+                    if(timeM<10){timeM ="0" + timeM}
+            const time = `${getMonth}/${getDate}   ${timeH}:${timeM}`;         //時間：分を"time"に入れる
         
-     const msg = {
-         imgpath:path,
-        imgUrl: imgUrl, 
-        uname: uname,
-        text: text,
-        time: time
-    };
-    refText.push(msg);
+            const msg = {
+                
+                imgpath:path,
+                imgUrl: imgUrl, 
+                uname: uname,
+                text: text,
+                time: time
+            };
+            refText.push(msg);
    
-        }
-           
-        );
+        });
     });
     
     
@@ -103,9 +139,11 @@ var firebaseConfig = {
 
 // 受信
 refText.on("child_added",function(data){
-    console.log(data)
+    console.log(data);
+
     const v = data.val();
     const k = data.key;
+    
     
     const dataImgpath = v.imgpath;
     const dataImg = v.imgUrl;
@@ -116,13 +154,20 @@ refText.on("child_added",function(data){
                      <div class="card">
                         <img src="${dataImg}" class="card-img-top" alt="">
                      <div class="card-body">
-                        <h5 class="card-title">${dataUname}</h5>
+                     <div id="map" style="width:100%; height:40px"></div>
+                        <h5 class="card-title">${dataUname} </h5>
                         <p class="card-text">${dataText}</p>
-                        <p class="card-text text-right"><small class="text-muted">${dataTime}</small> <button id="del" class="btn">×</button>  </p> 
+                       <p class="card-text text-right"><small class="text-muted">${dataTime}</small> <button id="del" class="btn">×</button>  </p> 
                      </div>
-                     </div> `
+                     </div>
+                     `
     
-    $("#output").prepend(dataAll);
+   
+
+        $("#output").prepend(dataAll);
+  
+  
+    
     
 $("#del").on("click",function(){
     flag = confirm("この投稿を削除しますか？");
@@ -139,6 +184,10 @@ $("#del").on("click",function(){
     }
    });
     
+
+
+
+  
 
 });
 
