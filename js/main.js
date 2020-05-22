ea770b0abb3ab32d61314a879d9506ba14c6ab8a
shona <f.mb@outlook.jp>
2020-05-22 23:36:15 +0900

    Map2
    それぞれのカードにMapを表示するように変更

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
+//HomeButton
 $("#btn-home").on("click",function(){
     location.reload();
 })
-
-
 
 
 // Your web app's Firebase configuration
@@ -15,69 +14,56 @@ var firebaseConfig = {
     messagingSenderId: "45049544375",
     appId: "1:45049544375:web:6df75ca8a1d90ab9711732"
   };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
-    // Get a reference to the storage service, which is used to create references in your storage bucket
-    var storage = firebase.storage();
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const refText = firebase.database().ref();
 
-
-
+// FirebaseSet
+var storage = firebase.storage();
    
 
-      
 
-    let fileButton = document.querySelector("#fileButton")
 
-    fileButton.addEventListener("change",function(e){
-        
-    });
-
-  const refText = firebase.database().ref();
   
- 
-
+//getGPS by exif-API
   function exif() {
-    //exif処理
-    var file_input = $('#fileButton');
-        console.log(file_input);
-    
-    var file = file_input[0].files[0];
-    // EXIF.getDataでexif情報を解析
+    return new Promise((resolve,reject) =>{ //同期化処理
+        var file_input = $('#fileButton');
+        var file = file_input[0].files[0];
+        // EXIF.getDataでexif情報を解析
         EXIF.getData(file, function() {
-            let data = EXIF.getAllTags(this);
-            console.log(data);
-        
-        let lat0 = data.GPSLatitude[0].numerator;
-        let lat1 = data.GPSLatitude[1].numerator;
-        let lat2 = data.GPSLatitude[2].numerator ;
-
-        let lat1A = lat1 / 60 
-        let lat2A = lat2 / 360000
-        
-        let lat = lat0 + lat1A + lat2A
-        console.log(lat);
-
-
-          
-        let lon0 = data.GPSLongitude[0].numerator;
-        let lon1 = data.GPSLongitude[1].numerator;
-        let lon2 = data.GPSLongitude[2].numerator;
-
-        let lon1A = lon1 / 60 
-        let lon2A = lon2 / 360000
-
-        let lon = lon0 + lon1A + lon2A
-        console.log(lon);
-        return {lat: lat, lon:lon};
-})
+            let gpsData = EXIF.getAllTags(this);
+            
+            //lat
+            let lat0 = gpsData.GPSLatitude[0].numerator;
+            let lat1 = gpsData.GPSLatitude[1].numerator;
+            let lat2 = gpsData.GPSLatitude[2].numerator ;
+                //60進数→10進数
+                let lat1A = lat1 / 60 
+                let lat2A = lat2 / 360000
+                let lat = lat0 + lat1A + lat2A
+            
+            //lon
+            let lon0 = gpsData.GPSLongitude[0].numerator;
+            let lon1 = gpsData.GPSLongitude[1].numerator;
+            let lon2 = gpsData.GPSLongitude[2].numerator;
+                //60進数→10進数
+                let lon1A = lon1 / 60 
+                let lon2A = lon2 / 360000
+                let lon = lon0 + lon1A + lon2A
+            
+            resolve([lat,lon]);
+        })
+    })
+    
 }
 
 
-//  ボタン送信
+// putCardInfo
+//selctFile
+let fileButton = document.querySelector("#fileButton")
     fileButton.addEventListener("change",function(e){   //ファイルを選択→送信でput〇
-        
-        //プレビュー
+        //preView
         $('img').remove();
         var file = $(this).prop('files')[0];
         if(!file.type.match('image.*')){
@@ -91,99 +77,116 @@ var firebaseConfig = {
         $("file-label").html(e.target.files[0])
 
         
+//Put
+$("#send").on("click",function(){
         
-
-
-       
-    $("#send").on("click",function(){
-        
-    //画像うｐ
-    //ファイル名を取得
-    let file = e.target.files[0];
+    //imgData
+        //getImgData
+        let file = e.target.files[0];
         
         
-    //storage ref を作成 //ファイルのアップロード
-    let storageRef = firebase.storage().ref('shon_wolrd/').child(file.name).put(file).then(snapshot => {
+        //storage ref を作成 //UP lode imgData
+        let storageRef = firebase.storage().ref('shon_wolrd/').child(file.name).put(file).then(snapshot => {
         let imgpath = firebase.storage().ref('shon_wolrd/').child(file.name).fullPath
-       
-        let url = snapshot.ref.getDownloadURL().then(url =>{
-           
-           
-            const path = file.name;
-            const imgUrl = url;
+    
+            //get imgURL
+            let url = snapshot.ref.getDownloadURL().then(url =>{
 
-            const uname = $("#uname").val();
-            const text = $("#text").val();
+                //get img GPS
+                exif().then(function(gps){
+                    let lat = gps[0]
+                    let lon = gps[1]
+                    //GPSData
+                    const la = lat;
+                    const lo = lon;
+                     //imgData
+                    const path = file.name;
+                    const imgUrl = url;
 
-            let now = new Date();  //Time取得
-                let getMonth = now.getMonth();
-                let getDate = now.getDate();
-                let timeH = now.getHours();     //時間
-                let timeM = now.getMinutes();      //分
-                    if(timeH<10){timeH ="0" + timeH}     //数字が一桁の場合０をつける。
-                    if(timeM<10){timeM ="0" + timeM}
-            const time = `${getMonth}/${getDate}   ${timeH}:${timeM}`;         //時間：分を"time"に入れる
-        
-            const msg = {
-                
+    //textData              
+    const uname = $("#uname").val();
+    const text = $("#text").val();      
+
+    //dateData
+    let now = new Date();  //Time取得
+        let getMonth = now.getMonth();
+        let getDate = now.getDate();
+        let timeH = now.getHours();     //時間
+        let timeM = now.getMinutes();      //分
+            if(timeH<10){timeH ="0" + timeH}     //数字が一桁の場合０をつける。
+            if(timeM<10){timeM ="0" + timeM}
+        const time = `${getMonth}/${getDate}   ${timeH}:${timeM}`;         //時間：分を"time"に入れる            
+   
+    //sendData
+    const msg = {
+                lat: la,
+                lon: lo,
                 imgpath:path,
                 imgUrl: imgUrl, 
                 uname: uname,
                 text: text,
                 time: time
-            };
-            refText.push(msg);
-   
+            };       
+    refText.push(msg);
+        
+                })
+            });
         });
-    } );
     });
-    
-    
-    
+});//rink at change
+
        
-    });
+
     
 
 
 
 
-// 受信
+// Receive
 refText.on("child_added",function(data){
     console.log(data);
 
-    const v = data.val();
-    const k = data.key;
+    const val = data.val();
+    const key = data.key;
     
-    
-    const dataImgpath = v.imgpath;
-    const dataImg = v.imgUrl;
-    const dataTime = v.time;
-    const dataUname = v.uname;
-    const dataText = v.text;
+    const lat = val.lat;
+    const lon = val.lon;
+    const dataImgpath = val.imgpath;
+    const dataImg = val.imgUrl;
+    const dataTime = val.time;
+    const dataUname = val.uname;
+    const dataText = val.text;
     const dataAll = `
-                     <div class="card">
-                        <img src="${dataImg}" class="card-img-top" alt="">
-                     <div class="card-body">
-                     
-                        <button type="button" id="btnmap" class="btn"  data-toggle="modal" data-target="#Modal2">...</button>
-                        <h5 class="card-title">${dataUname} </h5>
-                        <p class="card-text">${dataText}</p>
-                       <p class="card-text text-right"><small class="text-muted">${dataTime}</small> <button id="del" class="btn">×</button>  </p> 
+                    <div class="card">
+                            <img src="${dataImg}" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <button type="button" id="btnmap" class="btn"  data-toggle="modal" data-target="#Modal2">...</button>
+                            <h5 class="card-title">${dataUname}</h5>
+                            <p class="card-text">${dataText}</p>
+                            <p class="card-text text-right">
+                                <small class="text-muted">${dataTime}</small> 
+                                <button id="del" class="btn">×</button>  
+                            </p> 
+                        </div>
                      </div>
-                     </div>
-                     <script>
-                     $("#btnmap").on("click",function(){
-                        var opts = {
-                            zoom: 10,
-                            center: new google.maps.LatLng(33.354747,130.265603)
-                          };
-                          var map = new google.maps.Map(document.getElementById("map"), opts);
-                    })
-                    </script>
-                     `
-    
-   
 
+                    <script>
+                        $("#btnmap").on("click",function(){
+                            var opts = {
+                                zoom: 17,
+                                center: new google.maps.LatLng(${lat},${lon})
+                            };
+                            var map = new google.maps.Map(document.getElementById("map"), opts);
+
+                            marker = new google.maps.Marker({ // マーカーの追加
+                                animation: google.maps.Animation.DROP,
+                                position: new google.maps.LatLng(${lat},${lon}), // マーカーを立てる位置を指定
+                                map: map // マーカーを立てる地図を指定
+                            });
+                        })
+                    </script>`
+                     
+        //Output Card            
         $("#output").prepend(dataAll);
   
   
@@ -191,23 +194,17 @@ refText.on("child_added",function(data){
     
 $("#del").on("click",function(){
     flag = confirm("この投稿を削除しますか？");
-    if(flag == true){
-        console.log(data.key);
-       console.log(dataImgpath);
-        firebase.database().ref(data.key).remove().then(function(){
+    console.log(data.key);
+    console.log(dataImgpath);
+        if(flag == true){
+            firebase.database().ref(data.key).remove().then(function(){
             firebase.storage().ref('shon_wolrd/').child(dataImgpath).delete();
-            
             location.reload();
-        });
-    }else{
-
-    }
-   });
+            });
+        }
+});
     
 
-
-
-  
 
 });
 
